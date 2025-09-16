e3d3802f7eb81ea25859a7de6ae1f2f9a2a2ffa0
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-16 11:55:10 +0200

    imported middlewares

--- bonzai-api/functions/postBooking/index.mjs
+++ bonzai-api/functions/postBooking/index.mjs
@@ -1,8 +1,9 @@
 import middy from "@middy/core";
 import httpJsonBodyParser from "@middy/http-json-body-parser";
 import { sendResponse } from "../../responses/index.mjs";
-// import { errorHandler } from "../../middlewares/errorHandler.mjs";
+import { errorHandler } from "../../middlewares/errorHandler.mjs";
 import { addBooking } from "../../services/bookings.mjs";
+import { validateBooking } from "../../middlewares/validateBooking.mjs";
 
 //Länkat till errorHandler som ännu inte är skapad
 
@@ -13,5 +14,7 @@ export const handler = middy(async (event) => {
   } else {
     return sendResponse(404, { success: false, message: "Room could not be booked" });
   }
-}).use(httpJsonBodyParser());
-// .use(errorHandler());
+})
+  .use(httpJsonBodyParser())
+  .use(validateBooking())
+  .use(errorHandler());
