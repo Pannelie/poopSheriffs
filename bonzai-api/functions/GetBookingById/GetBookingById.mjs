24516eb5d48d175bf6598bcd5833e1a983983c09
Welene <helenetheo@live.no>
2025-09-16 13:07:19 +0200

    Added GetBookingById function -- error 500, can't find error in this code

--- bonzai-api/functions/GetBookingById/GetBookingById.mjs
+++ bonzai-api/functions/GetBookingById/GetBookingById.mjs
@@ -1,1 +1,29 @@
 import { sendResponse } from '../../responses/index.mjs';
+
+export const handler = async (event) => {
+	try {
+		const { id } = event.pathParameters || '';
+
+		if (!id) {
+			return sendResponse(400, {
+				message:
+					'This ID does not exist. Please put on glasses and take a closer look! 8-)',
+			});
+		}
+
+		// booking is undefined right now, until I get it from the database later
+		const booking = undefined;
+
+		if (!booking) {
+			return sendResponse(404, {
+				message: `There is no booking with the id of ${id} here...? :---)`,
+			});
+		}
+		return sendResponse(200, booking);
+	} catch (error) {
+		return sendResponse(500, {
+			message:
+				'Something went EXTREMELY wrong here when you get an error 500!! -- no bookings, no nothing.',
+		});
+	}
+};
