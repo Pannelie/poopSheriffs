7c52c4bca7cfd2815b7ccc1ce572ec037142f67c
Welene <helenetheo@live.no>
2025-09-16 15:38:34 +0200

    Changed name of GetBookingById to index.mjs

--- bonzai-api/functions/GetBookings/index.mjs
+++ bonzai-api/functions/GetBookings/index.mjs
@@ -1,20 +1,25 @@
 import { sendResponse } from '../../responses/index.mjs';
+import { client } from '../../services/client.mjs';
+import { ScanCommand } from '@aws-sdk/client-dynamodb';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 
 export const handler = async (event) => {
 	try {
-		if (event.body) {
-			return sendResponse(200, event.body);
-			// returns everything inside the body of the object if it exists
+		const command = new ScanCommand({ TableName: 'bonzai-table' });
+		const result = await client.send(command);
+
+		if (!result.Items || result.Items.length === 0) {
+			return sendResponse(200, {
+				message:
+					"There are no bookings right now. Don't look so sad - I am sure there will be some later... :-)",
+			});
 		}
 
-		// sendResponse checks the body, if it is empty then it returns a comforting message instead :-)
-		return sendResponse(200, {
-			message:
-				"There are no bookings right now. Don't look so sad - I am sure there will be some later... :-)",
-		});
+		// convert each DynamoDB item to plain JS object
+		const bookings = result.Items.map((item) => unmarshall(item));
+
+		return sendResponse(200, bookings);
 	} catch (error) {
 		return sendResponse(500, { message: error.message });
 	}
 };
-
-// TEST COMMENT HERE BLABLA
