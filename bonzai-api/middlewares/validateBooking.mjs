9cf858786c9e28ddfd1fd541bd7d35420dd7e24f
Sunsanee <3kdyhfe24.perssu@folkuniversitetet.nu>
2025-09-16 09:46:00 +0200

    Middlewareerrorhandler
    Signed-off-by: Sunsanee <3kdyhfe24.perssu@folkuniversitetet.nu>

---
+++ bonzai-api/middlewares/validateBooking.mjs
@@ -0,0 +1,13 @@
+import { bookingSchema } from "../models/bookingSchema.mjs";
+
+export const validateBooking = () => ({
+    befort : (handler) => {
+        const { error, value } = bookingSchema.validate(handler.event.body);
+        console.log('Error i middleware:', error);
+        console.log('Error i middleware:', value);
+        if(error) {
+            throw new Error(error.details[0].message);
+        }
+            
+    }
+});
