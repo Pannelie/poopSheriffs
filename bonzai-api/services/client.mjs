d0e4899416067dc023bad93745ae050eb4fe8c8e
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-17 13:28:22 +0200

    export for both client and docClient to make it easier merging code with groupmembers

--- bonzai-api/services/client.mjs
+++ bonzai-api/services/client.mjs
@@ -1,6 +1,6 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const client = new DynamoDBClient({ region: "eu-north-1" });
+export const client = new DynamoDBClient({ region: "eu-north-1" });
 
 export const docClient = DynamoDBDocumentClient.from(client);
