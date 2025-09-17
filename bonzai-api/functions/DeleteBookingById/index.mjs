42d14361eebf3e9d2403317d4aa632b2c42e099c
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-09-17 13:56:43 +0200

    add delete booking

--- bonzai-api/functions/DeleteBookingById/index.mjs
+++ bonzai-api/functions/DeleteBookingById/index.mjs
@@ -1,34 +1,44 @@
-import { client } from "../../dynamoClient/index.mjs";
+import { client } from "../../services/client.mjs";
 import { DeleteItemCommand } from "@aws-sdk/client-dynamodb";
 import { sendResponse } from "../../responses/index.mjs";
 
 export const handler = async (event) => {
   try {
-    const bookingId = event.pathParameters?.id;
+    const { id } = event.pathParameters || {};
 
-    if (!bookingId) {
-      return sendResponse(400, { message: 'Missing bookingId in path parameters' });
+    if (!id) {
+      return sendResponse(400, { 
+        message: "Missing bookingId in path parameters" 
+      });
     }
 
-    const del = new DeleteItemCommand({
-      TableName: 'bonzai-table',
-      Key: { pk: { S: `BOOKING#${bookingId}` } },
-      ReturnValues: 'ALL_OLD',
-    });
+    const booking = {
+      TableName: "bonzai-table",
+      Key: {
+        pk: { S: `BOOKING`},
+        sk: { S: id }
+      },
+      ReturnValues: "ALL_OLD",
+    };
 
-    const result = await client.send(del);
+    const result = await client.send(new DeleteItemCommand(booking));
+    const deletedBooking = result.Attributes;
 
-    if (!result.Attributes) {
-      return sendResponse(404, { message: `Booking ${bookingId} not found` });
+    if (!deletedBooking) {
+      return sendResponse(404, { 
+        message: `No booking found with id ${id}` 
+      });
     }
 
-    return sendResponse(200, {
-      message: 'Booking deleted successfully',
-      booking: result.Attributes,
+    return sendResponse(200, { 
+      message: "Booking deleted successfully", 
+      deletedBooking 
     });
 
-  } catch (err) {
-    console.error('Delete booking error:', err);
-    return sendResponse(500, { message: 'Internal Server Error' });
+  } catch (error) {
+    console.error("Error deleting booking:", error);
+    return sendResponse(500, { 
+      message: "Internal Server Error" 
+    });
   }
-};
+}
