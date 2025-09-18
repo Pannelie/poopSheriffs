993c33da805ab99b1daf7b3f6957d2734a0d24e8
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-18 21:20:38 +0200

    moved formatDateForResponse() into utils / date.mjs

--- bonzai-api/utils/date.mjs
+++ bonzai-api/utils/date.mjs
@@ -5,3 +5,8 @@ export const calculateCheckout = (checkIn, nights) => {
   checkOut.setDate(checkInDate.getDate() + Number(nights));
   return checkOut.toISOString();
 };
+
+export const formatDateForResponse = (date) => {
+  if (!date) return null;
+  return new Date(date).toISOString().split("T")[0]; // YYYY-MM-DD now and not YYYY-MM-DD.t000.0.00.000 like before
+};
