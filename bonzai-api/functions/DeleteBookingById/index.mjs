856d2fcbbdda9cc450347b69543656dec0d71c43
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-09-18 15:21:56 +0200

    fix delete booking

--- bonzai-api/functions/DeleteBookingById/index.mjs
+++ bonzai-api/functions/DeleteBookingById/index.mjs
@@ -4,26 +4,32 @@ import { deleteBooking } from "../../services/bookings.mjs";
 export const handler = async (event) => {
   try {
     console.log("Event:", JSON.stringify(event));
+    // Extract bookingId from path parameters
     const bookingId = event.pathParameters?.id;
 
+    // Validate bookingId
     if (!bookingId) {
       return sendResponse(400, { 
         message: "Missing bookingId" 
       });
     }
 
+    // Delete booking
     const deletedBooking = await deleteBooking(bookingId);
 
+    // If no booking found to delete
     if (!deletedBooking) {
       return sendResponse(404, { 
         message: `No booking found with id ${bookingId}` 
       });
     }
+    // Delete successful
     return sendResponse(200, { 
       message: "Booking deleted successfully", 
-      deletedBooking 
+      deletedBooking: deletedBooking
     });
   } catch (error) {
+    // Error handling
     console.error("Error in handler:", error);
     return sendResponse(500, { 
       message: "Internal Server Error",
