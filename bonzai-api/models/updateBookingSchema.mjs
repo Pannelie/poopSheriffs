512fba1e9fd0c392cee64b5876b29031dab20f81
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-19 13:03:17 +0200

    added .min(1) in bookingUpdateSchema. Mimimum of one field has to be filled

--- bonzai-api/models/updateBookingSchema.mjs
+++ bonzai-api/models/updateBookingSchema.mjs
@@ -4,14 +4,16 @@ export const bookingUpdateSchema = Joi.object({
   name: Joi.string().min(3).max(100),
   email: Joi.string().email(),
   guests: Joi.number().min(1),
-  rooms: Joi.array().items(
-    Joi.object({
-      roomType: Joi.string().valid("single", "double", "suite"),
-      amount: Joi.number().min(1),
-    })
-  ),
+  rooms: Joi.array()
+    .min(1)
+    .items(
+      Joi.object({
+        roomType: Joi.string().valid("single", "double", "suite"),
+        amount: Joi.number().min(1),
+      }).min(1)
+    ),
   checkIn: Joi.date().greater("now"),
   nights: Joi.number().min(1),
   checkOut: Joi.date().iso(),
-  totalPrice: Joi.number().min(0)
-});
+  totalPrice: Joi.number().min(0),
+}).min(1);
