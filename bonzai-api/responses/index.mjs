94ac7460a68a75186ce5240da4a296ffccd71a92
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-19 13:02:48 +0200

    created formatBookingResponse in responses

--- bonzai-api/responses/index.mjs
+++ bonzai-api/responses/index.mjs
@@ -6,3 +6,17 @@ export const sendResponse = (code, data) => {
     }),
   };
 };
+
+// Skapar ett “rent” booking-objekt för API-respons
+export const formatBookingResponse = (item) => ({
+  bookingId: item.bookingId,
+  name: item.name,
+  email: item.email,
+  guests: item.guests,
+  rooms: item.rooms,
+  totalRooms: item.totalRooms,
+  totalPrice: item.totalPrice,
+  checkIn: item.checkIn,
+  checkOut: item.checkOut,
+  nights: item.nights,
+});
